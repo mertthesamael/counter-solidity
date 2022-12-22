6fe24a4b45bba3384404b842f6a4c9bedfce8947
Merto <mertenercan@gmail.com>
2022-12-22 06:11:25 +0300

    -added loading spinner

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Flex, Text } from '@chakra-ui/layout';
 import abi from "./utils/abi.json"
 import { ethers } from "ethers";
 import { useEffect, useState } from 'react';
-import { Button, ChakraProvider } from '@chakra-ui/react';
+import { Button, ChakraProvider, Spinner } from '@chakra-ui/react';
 import { useToast } from '@chakra-ui/react'
 function App() {
   const toast = useToast()
   const [number, setNumber] = useState()
   const [logged,setLogged] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   //Function for fetching smart contract
   const fetchCa =  async() => {
@@ -27,20 +28,22 @@ function App() {
     const contract = new ethers.Contract("0x7DA2c7cbfA69347bAb8bD403DD313aB174E695F9", abi.abi, provider)
     contract.on("Change", (num) => {
         setNumber(num.toNumber())
-      
+        setLoading(false)
     })
 }
 
   //Increasing the value on blockchain
   const inc = async () => {
     try{
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
       const signer = provider.getSigner();
       const contract = new ethers.Contract("0x7DA2c7cbfA69347bAb8bD403DD313aB174E695F9", abi.abi, signer)
       await contract.increase().then(res=> console.log(res.value.toNumber()))
       const num = await contract.num()
       await setNumber(num.toNumber())
       checkEvents()
+      setLoading(true)
+
       
     }catch(err){
       console.log(err)
@@ -59,11 +62,13 @@ function App() {
     try{
       const provider = new ethers.providers.Web3Provider(window.ethereum)
       const signer = provider.getSigner();
-      const contract = new ethers.Contract("0x7DA2c7cbfA69347bAb8bD403DD313aB174E695F9", abi.abi, signer)   
+      const contract = new ethers.Contract("0x7DA2c7cbfA69347bAb8bD403DD313aB174E695F9", abi.abi, signer)
       await contract.decrease().then(res=> console.log(res.value.toNumber()))
       const num = await contract.num()
       await setNumber(num.toNumber())
       checkEvents()
+      setLoading(true)
+
       
     }catch(err){
       console.log(err)
@@ -98,7 +103,8 @@ useEffect(() => {
   return (
     <ChakraProvider>
     <Flex gap='2rem' justifyContent='center' flexDir='column' alignItems='center' h='100vh' w='100%' backgroundImage='linear-gradient( 94.3deg,  rgba(26,33,64,1) 10.9%, rgba(81,84,115,1) 87.1% )'>
-      <Text color='white' fontSize='15rem'>{number}</Text>
+      {<Spinner color='#FFBF00' thickness='8px' h='6rem' w='6rem'></Spinner>}
+      <Text color='white' textAlign='center' fontSize='15rem'>{number}</Text>
       <Text color='white' textAlign='center' fontSize='2rem'>You need to be on the Mumbai-Testnet to interact with the contract</Text>
       <Flex gap='2rem'>
       <Button p='2rem' onClick={inc} color='white' colorScheme='green'>Increase</Button>
